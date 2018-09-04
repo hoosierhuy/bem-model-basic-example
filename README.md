f2a94a931615a36af4877218a7f62213aad960de
Huy Nguyen <huy@Huys-iMac.local>
2018-09-04 18:38:04 -0400

    Initial commit of SASS branch

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ http://getbem.com/
 
 This is a basic example of the BEM model, don't worry about the styles, just focus on the structure of the markup and the relative "flatness" of the CSS.  Adapt and modify this pattern as you see fit.
 
-To run: npm i, and gulp command.
+To run: npm i, and gulp command.  In this branch, I used SASS.
 
 tl;dr
 Even though this is a BEM demo, I've set this up for general CSS dev without a CSS framework, you might actually not need a framework, depending on your project scope and time horizon.
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+
+// These 2 tasks can may be re-factored for improved efficiency.
+gulp.task('scss', () => {
+     gulp.src(['scss/index.scss', './**/*.scss','!node_modules', '!node_modules/**'])
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(gulp.dest("./css"));
+});
 
 gulp.task('serve', () => {
     browserSync.init({
         server: './'
     });
 
+    gulp.watch(['scss/index.scss','./**/*.scss'], ['scss']).on('change', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch(['./*.css', './**/*.css']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve', 'scss']);
